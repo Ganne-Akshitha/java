20c56d56df3923893fc519fb7be54db9114b381b
Ganne-Akshitha <142087735+Ganne-Akshitha@users.noreply.github.com>
2023-09-09 15:32:10 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class datatype
+{
+public static void main(String[] args)
+{
+int x;
+String name;
+double y;
+Scanner sc=new Scanner(System.in);
+name=sc.next();
+x=sc.nextInt();
+
+y=sc.nextDouble();
+
+}
+}
